bc1cc250efca2d70155ef62fbe680940f806b5cf
antho <antho@192.168.0.23>
2020-05-20 23:54:11 +0200

    Commit and Push all files for SNAPSHOT 1.0 Backup

---
+++ ProductManager/src/main/java/net/codejava/ProductManager/ProductRepository.java
@@ -0,0 +1,9 @@
+package net.codejava.ProductManager;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
+
+// MY REPOSITORY CLASS called: ProductRepository (20:00min)
